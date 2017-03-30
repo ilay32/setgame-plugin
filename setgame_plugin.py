7f007ff979fcea4e695028a35f9c6e262567dd4d
ilay <silayster@gmail.com>
2017-03-30 18:42:21 +0300

    Change The Plugin Name

--- setgame_plugin.py
+++ setgame_plugin.py
@@ -4,36 +4,39 @@ from libqtopensesame.items.qtautoplugin import qtautoplugin
 from openexp.canvas import canvas
 from openexp.log import log
 from openexp.mouse import mouse
-from playsets import play 
-import os,re
+import os,re,sys
+sys.path.insert(0,os.path.dirname(__file__)+'/setgame')
+from playset import play
 
-class setgame(item):
+class setgame_plugin(item):
     description = u'An example game  plug-in'
     ordpat = re.compile(".*_(\d{1,2})$")
     def reset(self):
         self.var.duration = 1000*60*2
-        self.experiment.var.sets_found = 0
+        self.var.sets_found = 0
+
 
     def prepare(self):
         item.prepare(self)
         self.c = canvas(self.experiment)
-        ordm = setgame.ordpat.match(self.name)
+        ordm = setgame_plugin.ordpat.match(self.name)
         self.ord = 0 if ordm is None else int(ordm.group(1))
+        
+    #def run(self):
+    #    self.set_item_onset(self.playsets()) 
 
     def run(self):
-        self.set_item_onset(self.playsets()) 
-
-    def playsets(self):
         mmouse = mouse(self.experiment)
         mmouse.show_cursor()
         self.c.show()
-        gameresults = play(self.experiment.window,self.var.duration)
-                v = gameresults['found']
-        #self.experiment.var[pref+'__wrong_attempts'] = gameresults['wrong']
+        fullscreen = False if self.experiment.var.fullscreen == "no" else True
+        gameresults = play(self.experiment.window,self.var.duration,fullscreen)
+        self.var.sets_found = gameresults['found']
+        self.experiment.var.score = self.var.sets_found
 
-class qtsetgame(setgame, qtautoplugin):
+class qtsetgame_plugin(setgame_plugin, qtautoplugin):
     def __init__(self, name, experiment, script=None):
-        setgame.__init__(self, name, experiment, script)
+        setgame_plugin.__init__(self, name, experiment, script)
         qtautoplugin.__init__(self, __file__)
 
     def init_edit_widget(self):
