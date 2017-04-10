95ff4613d402785988ac6adfd47c5087bf7870be
ilay <silayster@gmail.com>
2017-04-10 14:28:26 +0300

    Smarter Variable Handling  + new commit in setgame submodule

--- setgame_plugin.py
+++ setgame_plugin.py
@@ -4,36 +4,39 @@ from libqtopensesame.items.qtautoplugin import qtautoplugin
 from openexp.canvas import canvas
 from openexp.log import log
 from openexp.mouse import mouse
-import os,re,sys
+import os,re,sys,pprint
 sys.path.insert(0,os.path.dirname(__file__)+'/setgame')
-from playset import play
+from playset import play,practice
 
 class setgame_plugin(item):
     description = u'An example game  plug-in'
     ordpat = re.compile(".*_(\d{1,2})$")
     def reset(self):
         self.var.duration = 1000*60*2
-        self.var.sets_found = 0
-
+        self.var.mode = "play"
+        self.var.lang = "he" #one may add a language option in the gui and...
 
     def prepare(self):
         item.prepare(self)
         self.c = canvas(self.experiment)
         ordm = setgame_plugin.ordpat.match(self.name)
         self.ord = 0 if ordm is None else int(ordm.group(1))
-        
-    #def run(self):
-    #    self.set_item_onset(self.playsets()) 
+        self.runner = eval(self.var.mode)
 
     def run(self):
         mmouse = mouse(self.experiment)
         mmouse.show_cursor()
         self.c.show()
         fullscreen = False if self.experiment.var.fullscreen == "no" else True
-        gameresults = play(self.experiment.window,self.var.duration,fullscreen)
-        self.var.sets_found = gameresults['found']
-        self.experiment.var.score = self.var.sets_found
+        gameresults = self.runner(self.experiment.window,self.var.duration,self.lang,fullscreen)
+        self.set_vars(gameresults)    
 
+    def set_vars(self,data):
+        pprint.pprint(data)
+        for k,v in data.iteritems():
+            self.experiment.var.set(k,v)
+
+            
 class qtsetgame_plugin(setgame_plugin, qtautoplugin):
     def __init__(self, name, experiment, script=None):
         setgame_plugin.__init__(self, name, experiment, script)
