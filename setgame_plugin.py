6c578188d81313e4eead21cf37b44ba25dfdb9c3
ilay <silayster@gmail.com>
2017-08-03 19:50:28 +0300

    Change the Game Running Method

--- setgame_plugin.py
+++ setgame_plugin.py
@@ -6,35 +6,47 @@ from openexp.log import log
 from openexp.mouse import mouse
 import os,re,sys,pprint
 sys.path.insert(0,os.path.dirname(__file__)+'/setgame')
-from playset import play,practice
+from playset import RunGame
 
 class setgame_plugin(item):
     description = u'An example game  plug-in'
-    ordpat = re.compile(".*_(\d{1,2})$")
+    #ordpat = re.compile(".*_(\d{1,2})$")
     def reset(self):
         self.var.duration = 1000*60*2
         self.var.mode = "play"
         self.var.lang = "he" #one may add a language option in the gui and...
 
     def prepare(self):
+        self.experiment.var.personal_record = 0
         item.prepare(self)
         self.c = canvas(self.experiment)
-        ordm = setgame_plugin.ordpat.match(self.name)
-        self.ord = 0 if ordm is None else int(ordm.group(1))
-        self.runner = eval(self.var.mode)
+        #ordm = setgame_plugin.ordpat.match(self.name)
+        #self.ord = 0 if ordm is None else int(ordm.group(1))
+        self.fullscreen = False if self.experiment.var.fullscreen == "no" else True
+        
 
     def run(self):
         mmouse = mouse(self.experiment)
         mmouse.show_cursor()
         self.c.show()
-        fullscreen = False if self.experiment.var.fullscreen == "no" else True
-        gameresults = self.runner(self.experiment.window,self.var.duration,self.lang,fullscreen)
-        self.set_vars(gameresults)    
+        runner = RunGame(mode=self.var.mode, \
+            frame  = self.experiment.window, \
+            duration = self.var.duration, \
+            language = self.lang, \
+            fullscreen = self.fullscreen
+        )
+
+        gameresults = runner.run()()
+        pr = runner.feedback_screen(gameresults,self.experiment.var.personal_record)
+        self.experiment.var.set('personal_record',pr)
+        self.set_vars(gameresults)
 
     def set_vars(self,data):
         pprint.pprint(data)
         for k,v in data.iteritems():
             self.experiment.var.set(k,v)
+            if k == 'score' and v > self.experiment.var.personal_record:
+                self.experiment.var.personal_record = v
 
             
 class qtsetgame_plugin(setgame_plugin, qtautoplugin):
